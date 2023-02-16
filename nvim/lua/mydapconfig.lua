321a121927feae6354fcc5369d464bb8ec59ebda
SeokguKim <43718966+SeokguKim@users.noreply.github.com>
2023-02-16 17:04:07 +0900

    Create mydapconfig.lua

---
+++ nvim/lua/mydapconfig.lua
@@ -0,0 +1,41 @@
+local M = {}
+
+function M.setup()
+	local dap = require('dap')
+	dap.adapters.lldb = {
+		type = 'executable',
+		command = 'C:/Program Files/LLVM/bin/lldb-vscode',
+		name = "lldb"
+	}
+
+	dap.configurations.cpp = {
+		{
+			name = "Launch",
+			type = "lldb",
+			request = "launch",
+			program = "C:/tools/neovim/nvim-win64/bin/Debug/Debug/Problem.exe",
+			cwd = '${workspaceFolder}',
+			stopOnEntry = false,
+			args = {},
+			runInTerminal = false,
+			preLaunchTask = "build",
+		}
+	}
+
+	dap.adapters.python = {
+	  type = 'executable',
+	  command = 'python',
+	  args = { '-m', 'debugpy.adapter' },
+	}
+	dap.configurations.python = {
+	  {
+		type = 'python',
+		request = 'launch',
+		name = 'Launch file',
+		program = '${file}',
+		pythonPath = 'C:/Users/rokja/AppData/Local/Programs/Python/Python311/python.exe',
+	  }
+	}
+end
+
+return M
