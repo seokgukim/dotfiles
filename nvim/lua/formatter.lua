3d8cf46ffa2a3d764521093b952f5cb9c0659755
seokgukim <rokja97@naver.com>
2025-08-20 14:23:13 +0900

    feat: add formatters

--- nvim/lua/formatter.lua
+++ nvim/lua/formatter.lua
@@ -2,6 +2,20 @@ local M = {}
 
 function M.setup()
     require("conform").setup({
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "black" },
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            html = { "prettier" },
+            css = { "prettier" },
+            json = { "prettier" },
+            markdown = { "prettier" },
+            yaml = { "prettier" },
+            cpp = { "clang_format" },
+            c = { "clang_format" },
+        },
+
         format_on_save = {
             lsp_fallback = true,
             timeout_ms = 1000,
