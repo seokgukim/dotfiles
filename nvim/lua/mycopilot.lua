aa7bf14f485c2e838e83d4829f50b7c7f0b46e12
seokgukim <rokja97@naver.com>
2025-08-20 14:24:36 +0900

    feat: copilot toggle keymap

--- nvim/lua/mycopilot.lua
+++ nvim/lua/mycopilot.lua
@@ -1,12 +1,19 @@
 local M = {}
 
 function M.setup()
-  vim.keymap.set("n", "<leader>cp", function()
-    vim.cmd("Copilot panel")
-  end, { desc = "Open Copilot Panel", silent = true })
-  vim.keymap.set("i", "<C-l>", "<Plug>(copilot-next)", { desc = "Next Copilot Suggestion", noremap = true })
-  vim.keymap.set("i", "<C-h>", "<Plug>(copilot-previous)", { desc = "Previous Copilot Suggestion", noremap = true })
-  vim.keymap.set("i", "<leader><Tab>", "<Plug>(copilot-accept)", { desc = "Accept Copilot Suggestion", noremap = true })
+	-- Copilot toggle functionality
+	vim.keymap.set("n", "<leader><f1>", function()
+		if vim.g.copilot_enabled then
+			vim.g.copilot_enabled = false
+			print("Copilot disabled")
+		else
+			vim.g.copilot_enabled = true
+			print("Copilot enabled")
+		end
+	end, { desc = "Toggle Copilot" })
+
+	-- Disable copilot by default
+	vim.g.copilot_enabled = false
 end
 
 return M
